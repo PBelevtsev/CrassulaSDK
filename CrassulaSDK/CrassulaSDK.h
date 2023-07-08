d7bdd557f4938b3f933a8f6e0738f118a46c143c
Pavel Belevtsev <pbelevtsev@hotmail.com>
2023-07-09 00:05:58 +0300

    Initial commit

---
+++ CrassulaSDK/CrassulaSDK.h
@@ -0,0 +1,18 @@
+//
+//  CrassulaSDK.h
+//  CrassulaSDK
+//
+//  Created by Pavel Belevtsev on 08.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CrassulaSDK.
+FOUNDATION_EXPORT double CrassulaSDKVersionNumber;
+
+//! Project version string for CrassulaSDK.
+FOUNDATION_EXPORT const unsigned char CrassulaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrassulaSDK/PublicHeader.h>
+
+
